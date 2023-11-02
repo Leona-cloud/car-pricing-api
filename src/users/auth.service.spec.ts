47487c5afe78307618246744420c9fe8aa2db9ef
Leona-cloud <chisomokafor038@gmail.com>
2023-11-03 00:47:44 +0100

    chore: writing tests

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,15 +1,17 @@
 import { Test } from '@nestjs/testing';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     //create a fake copy of userservice
 
-    const fakeUserService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -20,7 +22,7 @@ describe('AuthService', () => {
         AuthService,
         {
           provide: UsersService,
-          useValue: fakeUserService,
+          useValue: fakeUsersService,
         },
       ],
     }).compile();
@@ -28,7 +30,42 @@ describe('AuthService', () => {
     service = module.get(AuthService);
   });
 
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
+
+  it('creates a new user witha salted and hashed password', async () => {
+    const user = await service.signup('asdf@gmail.com', 'asdf');
+
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if a user signs up with an email in use', async (done) => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+      try {
+        await service.signup('asdf@asdf.com', 'asdf');
+      } catch (err) {
+       done()
+      }
+   
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.sigin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
+  });
+
 });
--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,44 +1,48 @@
-import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
-import { UsersService } from "./users.service";
-import { randomBytes, scrypt as _scrypt} from "crypto";
-import { promisify } from "util";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
-const scrypt = promisify(_scrypt)
+const scrypt = promisify(_scrypt);
 
 @Injectable()
-export class AuthService{
-    constructor(private usersService: UsersService){}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
 
-   async signup(email: string, password: string){
-        // check if email already exists
-        const users = await this.usersService.find(email);
-        if (users.length) {
-            throw new BadRequestException('email already in use')
-        };
-        // hash password
-        const salt = randomBytes(8).toString('hex');
-        const hash = (await scrypt(password, salt, 32)) as Buffer
-        const result =  salt + '.' + hash.toString('hex')
+  async signup(email: string, password: string) {
+    // check if email already exists
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email already in use');
+    }
+    // hash password
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
 
-        //create new user and save 
-        const user = await this.usersService.create(email, result)
-        // return user
-        return user;
+    //create new user and save
+    const user = await this.usersService.create(email, result);
+    // return user
+    return user;
+  }
+
+  async sigin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('user not found');
     }
 
-    async sigin(email: string, password: string ){
-        const [user]  = await this.usersService.find(email);
-        if(!user){
-            throw new NotFoundException('user not found')
-        };
+    const [salt, storedHash] = user.password.split('.');
 
-        const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
 
-        const hash = await scrypt(password, salt, 32) as Buffer;
-
-        if(storedHash !== hash.toString('hex')){
-            throw new BadRequestException('Invalid email or password')
-        }
-        return user;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Invalid email or password');
     }
+    return user;
+  }
 }
